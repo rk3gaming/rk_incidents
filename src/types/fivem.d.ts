eaed294db23e58684a0faced3b0af1173c3953d3
RK <rkfrmda3@gmail.com>
2025-06-18 21:51:11 -0500

    Delete src directory

--- src/types/fivem.d.ts
+++
@@ -1,5 +0,0 @@
-declare function LoadResourceFile(resourceName: string, fileName: string): string;
-declare function GetCurrentResourceName(): string;
-declare function IsControlJustReleased(padIndex: number, control: number): boolean;
-declare function IsPlayerAceAllowed(playerId: number, permission: string): boolean;
-declare function GetPlayerIdentifier(playerId: number, identifierType: number): string; 
